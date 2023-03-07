6f9f14bca7edc3c3b28ec0b3366f608abe0d3f3e
galenyjl <galenyjl@gmail.com>
2023-03-07 21:39:10 +1300

    specflow implementation

---
+++ February2023/February2023/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace February2023.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged into turnup portal successfully")]
+        public void GivenILoggedIntoTurnupPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I navigate to Time and Material page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I create a new time and material record")]
+        public void WhenICreateANewTimeAndMaterialRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
